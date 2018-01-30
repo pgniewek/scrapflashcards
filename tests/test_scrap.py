ab8b7e23f7b1198ce264ace27da44169c5ad2f9c
098799 <tomek.grining@gmail.com>
2018-01-30 14:26:27 +0100

    First crack at wiki's API -- getting category members

--- tests/test_scrap.py
+++ tests/test_scrap.py
@@ -1,8 +1,8 @@
 import pytest
 from mock import patch
+from scrapflashcards import scrap
 
 def test_init():
-    from scrapflashcards import scrap
     with patch.object(scrap, "main", return_value=42):
         with patch.object(scrap, "__name__", "__main__"):
             with patch.object(scrap.sys, 'exit') as mock_exit:
@@ -10,3 +10,6 @@ def test_init():
 
                 assert mock_exit.call_args[0][0] == 42
     
+def test_getCategoryMembers():
+    sampleResult = [{'title': 'Carmen Mauri', 'pageid': 19087166, 'ns': 0}]
+    assert scrap.getCategoryMembers("Category:History_of_Poland_during_the_Piast_dynasty", 1) == sampleResult
